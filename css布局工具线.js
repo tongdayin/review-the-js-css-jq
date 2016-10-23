7c62b8215f2fd2a8be60c08b1c02d5ddf30bc2f0
Longsheng <13410464746@163.com>
2016-10-23 21:59:42 +0800

    添加小道具

---
+++ css布局工具线.js
@@ -0,0 +1,44 @@
+var ckXian = function () {
+    var button = `<button class="xm" type="button" name="button">参考线</button>`
+    $('body').prepend(button)
+    var style =
+    `<style id="xm" media="screen">
+        div{border: 1px red dashed;}
+    </style>
+    `
+    var i = false
+    $(".xm").on('click',function () {
+        if (i) {
+            $("#xm").remove()
+            i = false
+        }else {
+            $("body").prepend(style)
+            i = true
+        }
+    })
+
+}
+ckXian ()
+var ckXian = function () {
+    var body = document.querySelector('body')
+    var style =
+    `<style id="xm" media="screen">
+        div{outline: 1px red dashed;}
+    </style>
+    `
+
+    i = false
+    body.addEventListener('keydown', function(event){
+        if (event.keyCode === 77 && event.ctrlKey) {
+            if (i) {
+                var styletog = document.querySelector('#xm')
+                styletog.remove()
+                i= false
+            }else {
+                body.insertAdjacentHTML('afterbegin', style);
+                i = true
+            }
+        }
+    })
+}
+ckXian ()
